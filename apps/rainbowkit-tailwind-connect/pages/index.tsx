8370a26ec958cf1ea06b7b05f0a0ce83a6cf0317
bao <baomai@berkeley.edu>
2024-05-27 00:40:38 -0700

    fixed workspace issues

--- apps/rainbowkit-tailwind-connect/pages/index.tsx
+++ apps/rainbowkit-tailwind-connect/pages/index.tsx
@@ -3,7 +3,6 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
