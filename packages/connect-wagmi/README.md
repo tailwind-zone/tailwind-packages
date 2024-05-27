d47c24523bdc6456c9ec27839e075f37e1440343
bao <baomai@berkeley.edu>
2024-05-26 22:19:07 -0700

    added @tailwindzone/connect-wagmi

---
+++ packages/connect-wagmi/README.md
@@ -0,0 +1,5 @@
+
+# Introduction
+TAILWIND is your Sei & Interchain wallet from the future. 
+See [docs](https://developer.tailwidn.zone/wallet-connect/evm/wagmi) 
+for more info.
---
+++ packages/connect-wagmi/chains/index.ts
@@ -0,0 +1,34 @@
+import { defineChain } from "viem"
+
+export const seimainnet = defineChain({
+  id: 531,
+  name: 'Sei Mainnet',
+  nativeCurrency: { name: 'Sei', symbol: 'SEI', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://evm-rpc.sei-apis.com'],
+    },
+  },
+})
+
+export const seiatlantic2 = defineChain({
+  id: 1328,
+  name: 'Sei Atlantic-2 Testnet',
+  nativeCurrency: { name: 'Sei', symbol: 'SEI', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://evm-rpc.atlantic-2.seinetwork.io'],
+    },
+  },
+})
+
+export const seiartic1 = defineChain({
+  id: 713715,
+  name: 'Sei Artic-1 Testnet',
+  nativeCurrency: { name: 'Sei', symbol: 'SEI', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://evm-rpc-arctic-1.sei-apis.com'],
+    },
+  },
+})
