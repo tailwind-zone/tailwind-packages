84149de381167d68dcbe8396efe2ee9a07391fe5
bao <baomai@berkeley.edu>
2024-05-27 20:02:13 -0700

    vite react tailwind connect

---
+++ apps/vite-react-tailwind-connect/src/App.tsx
@@ -0,0 +1,34 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { connect } from '@tailwindzone/connect'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={async () => await connect()}>
+          Connect TAILWIND
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
