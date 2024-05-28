57753487ed1037096a6724f0472c15f3845f2b5e
bao <baomai@berkeley.edu>
2024-05-27 19:16:17 -0700

    logic for connect-web3-react

--- packages/connect-web3-react/src/index.ts
+++ packages/connect-web3-react/src/index.ts
@@ -1,1 +1,9 @@
-export const initialized = 1;
+import { initializeConnector } from "@web3-react/core"
+import { TailwindConnector } from "./tailwind"
+
+const tailwindConnector = initializeConnector(actions => new TailwindConnector({ actions })) 
+
+export {
+  tailwindConnector,
+  TailwindConnector
+}
