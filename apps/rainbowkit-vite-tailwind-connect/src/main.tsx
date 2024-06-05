b7b1ba4a31d9810009dfeb9369aa197f95381ee6
bao <baomai@berkeley.edu>
2024-06-04 17:01:36 -0700

    basic rainbowkit setup

--- apps/rainbowkit-vite-tailwind-connect/src/main.tsx
+++ apps/rainbowkit-vite-tailwind-connect/src/main.tsx
@@ -2,9 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { defineChain } from 'viem';
+
+export const seimainnet = defineChain({
+  id: 1329,
+  name: 'Sei Mainnet',
+  nativeCurrency: { name: 'Sei', symbol: 'SEI', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['https://evm-rpc.sei-apis.com'],
+    },
+  },
+})
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [seimainnet],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+  ,
 )
