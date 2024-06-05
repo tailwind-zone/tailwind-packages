b2efe232e1a99c30a361a0f101ebd5560e0adc90
bao <baomai@berkeley.edu>
2024-06-04 17:01:36 -0700

    basic rainbowkit setup

--- apps/rainbowkit-vite-tailwind-connect/src/App.tsx
+++ apps/rainbowkit-vite-tailwind-connect/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,9 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <ConnectButton /> 
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
